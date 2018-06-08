3b4541afd8227edd782d4c8283f75ee646513781
MehSwitch <aymehaiDev@protonmail.com>
2018-06-08 14:44:04 -0400

    This is the full project for higher or lower

---
+++ app/src/main/java/com/example/aymen/myapplication3/CorrectGuessActivity.kt
@@ -0,0 +1,26 @@
+package com.example.aymen.myapplication3
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_correct_guess.*
+
+class CorrectGuessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_correct_guess)
+        playAgain()
+        exitGame()
+    }
+    fun playAgain(){
+        btnPlayAgain.setOnClickListener{
+            val intent = Intent("com.example.aymen.myapplication3.MainActivity")
+            startActivity(intent)
+        }
+    }
+    fun exitGame(){
+        btnExit.setOnClickListener{
+            this.finishAffinity()
+        }
+    }
+}
